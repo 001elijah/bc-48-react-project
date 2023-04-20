9002f79d860ff4d35171529fb884af44b172b232
GGalina <71042425+GGalina@users.noreply.github.com>
2023-04-20 23:00:45 +0400

    removed module imports

--- src/components/Header/Logo/Logo.jsx
+++ src/components/Header/Logo/Logo.jsx
@@ -1,14 +1,13 @@
 import { useMediaQuery } from 'react-responsive';
 import logoMobile from 'assets/img/logo-mobile.png';
 import logoTabletDesktop from 'assets/img/logo-desktop-tablet.png'
-import s from './Logo.module.scss';
 
 export const Logo = () => {
     const isDesktopOrLaptop = useMediaQuery({ query: '(min-width: 769px)' });
     const isMobile = useMediaQuery({ query: '(max-width: 768px)' });
 
     return (
-        <div className={s.Logo}>
+        <>
             {isMobile &&
                 <img
                     src={logoMobile}
@@ -25,6 +24,6 @@ export const Logo = () => {
                     alt="Finance logo"
                 />
             }
-        </div>
+        </>
     )
 };
--- src/components/Header/Navigation/Navigation.jsx
+++ src/components/Header/Navigation/Navigation.jsx
@@ -2,15 +2,14 @@ import { useSelector } from 'react-redux';
 import { selectAuthorized } from 'redux/selectors/authSelectors';
 import { AuthNav } from 'components/Header/AuthNav/AuthNav';
 import { UserBar } from 'components/Header/UserBar/UserBar';
-import s from './Navigation.module.scss';
 
 export const Navigation = () => {
     const isAuthorized = useSelector(selectAuthorized);
 
     return (
-        <div className={s.NavigationContainer}>
+        <>
             {!isAuthorized && <AuthNav />}
             {isAuthorized && <UserBar />}
-        </div>
+        </>
     );
 };
