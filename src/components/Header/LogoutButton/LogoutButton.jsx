3fc0fcf5f8b7e7b29df72ac2d4931070836ceb18
GGalina <71042425+GGalina@users.noreply.github.com>
2023-04-22 00:08:05 +0400

    fixed header

--- src/components/Header/LogoutButton/LogoutButton.jsx
+++ src/components/Header/LogoutButton/LogoutButton.jsx
@@ -1,13 +1,20 @@
 import { useDispatch } from 'react-redux';
 import { logout } from 'redux/operations/authOperations';
+import { useNavigate } from 'react-router-dom';
 import s from './LogoutButton.module.scss';
 import svg from 'assets/icons/sprite.svg';
 
 export const LogoutButton = () => {
     const dispatch = useDispatch();
+    const navigate = useNavigate();
+    
+    const handleLogout = () => {
+        dispatch(logout());
+        navigate('/');
+    }
 
     return (
-        <button className={s.Logout} type="button" onClick={() => dispatch(logout())}>
+        <button className={s.Logout} type="button" onClick={handleLogout}>
             Log out 
             <svg width="14" height="14" className={s.IconLogout}>
                 <use xlinkHref={`${svg}#icon-log-out`} />
