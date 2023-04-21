26545b07bb21c75bcaa24af250cbc32f5b27b162
Illia Zolotukha <job.illia.igorevich@gmail.com>
2023-04-21 20:54:03 +0300

    add validation of controlled inputs upon blur in FinanceForm.jsx

--- src/components/TextDataInput/TextDataInput.jsx
+++ src/components/TextDataInput/TextDataInput.jsx
@@ -1,7 +1,7 @@
 import s from './TextDataInput.module.scss';
 
 export const TextDataInput = ({ ...props }) => {
-    const { label, htmlFor, placeholder, hint = null, id, name, onChange, value } = props;
+    const { label, htmlFor, placeholder, hint = null, id, name, onChange, onBlur, value, fieldError, isFieldTouched } = props;
 
     return (
         <>
@@ -10,12 +10,14 @@ export const TextDataInput = ({ ...props }) => {
                     id={id}
                     name={name}
                     onChange={onChange}
+                    onBlur={onBlur}
                     value={value}
                     className={s.TextDataInput}
                     type="text"
                     placeholder={placeholder}
                 />
                 <label htmlFor={htmlFor}><span className={s.TextDataInputTitle}>{label}</span></label>
+                { isFieldTouched && fieldError && <p className={s.Error}>{fieldError}</p> }
                 {hint && <span className={s.Hint}>{hint}</span>}
             </div>
             
