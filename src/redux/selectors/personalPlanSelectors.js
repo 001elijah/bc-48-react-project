90a3e7f6fe364959bb89f98812407b1cf5172d56
Illia Zolotukha <job.illia.igorevich@gmail.com>
2023-04-22 14:00:11 +0300

    update plan selectors, fix bugs for plan page

--- src/redux/selectors/personalPlanSelectors.js
+++ src/redux/selectors/personalPlanSelectors.js
@@ -1,3 +1,5 @@
+import { createSelector } from "@reduxjs/toolkit";
+
 export const selectSalary = state => state.personalPlan.salary;
 export const selectPassiveIncome = state => state.personalPlan.passiveIncome;
 export const selectSavings = state => state.personalPlan.savings;
@@ -6,3 +8,16 @@ export const selectFootage = state => state.personalPlan.footage;
 export const selectProcent = state => state.personalPlan.procent;
 export const selectYear = state => state.personalPlan.year;
 export const selectMonth = state => state.personalPlan.month;
+
+export const selectPlan = createSelector([selectSalary, selectPassiveIncome, selectSavings, selectCost, selectFootage, selectProcent],
+(salary,
+            passiveIncome,
+            savings,
+            cost,
+            footage,
+    procent) => ({salary,
+            passiveIncome,
+            savings,
+            cost,
+            footage,
+            procent}));
