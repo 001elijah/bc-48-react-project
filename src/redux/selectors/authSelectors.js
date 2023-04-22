58816812d29f0889affb522e8d67da6fc756a8f7
Illia Zolotukha <job.illia.igorevich@gmail.com>
2023-04-22 12:05:15 +0300

    add selector isLoading

--- src/redux/selectors/authSelectors.js
+++ src/redux/selectors/authSelectors.js
@@ -2,3 +2,4 @@ export const selectAuthorized = state => Boolean(state.authorized.token);
 export const selectUser = state => state.authorized.user;
 export const selectUserToken = state => state.authorized.token;
 export const selectError = state => state.authorized.error;
+export const selectIsLoading = state => state.authorized.isLoading;
