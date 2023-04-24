12c1eeaa29187b1e2585ebbed8a76e521cf7e60f
GGalina <71042425+GGalina@users.noreply.github.com>
2023-04-24 13:33:13 +0400

    Fixes to styles

--- src/components/DynamicsComponents/Info/OneMoreSqMeter.jsx
+++ src/components/DynamicsComponents/Info/OneMoreSqMeter.jsx
@@ -11,7 +11,7 @@ const OneMoreSqMeter = () => {
         </p>
         <p className={s.sum}>14 000 ₴</p>
       </div>
-      <img className={s.img} src={image} alt="Finance app" />
+      <img className={s.img} src={image} alt="Finance app" width='185' height='165'/>
     </div>
   );
 };
